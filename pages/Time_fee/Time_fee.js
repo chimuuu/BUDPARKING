9e845f3ccac31e6fa8d2de1b66b6fee2ceeb988e
chimuuu <w409048772@163.com>
2016-12-04 11:30:42 +0800

    Add files via upload
    各个页面文件

---
+++ pages/Time_fee/Time_fee.js
@@ -0,0 +1,95 @@
+// // Time_fee.js
+
+// var util = require('../../utils/util.js')
+
+  function formatTime(date) {
+    var year = date.getFullYear()
+    var month = date.getMonth() + 1
+    var day = date.getDate()
+
+    var hour = date.getHours()
+    var minute = date.getMinutes()
+    var second = date.getSeconds()
+
+    return "停车开始时间:" + [year, month, day].map(formatNumber).join('-') + ' ' + [hour, minute].map(formatNumber).join(':')
+    }
+
+function formatNumber(n) {
+  	n = n.toString()
+  	return n[1] ? n : '0' + n
+	}
+
+// 记录下停车开始时间
+function start_time_record(that){
+	that.setData({
+		start_time:formatTime(new Date()),
+	});
+}
+/** 
+计时功能模块
+//初始化开始时间为0毫秒
+
+/* 秒级计时 */
+var total_micro_second = 0;
+function timer_cost(that) {	
+
+  	  	that.setData({
+  		Timer:date_format(total_micro_second),
+		total_cost:Math.floor(total_micro_second/1000)
+  		});
+// 一个月
+		if (total_micro_second/1000/24/3600>=30) {	
+  			that.setData({
+  			Timer:"已经达到一个月期限"
+  		})
+	    }    
+setTimeout(function(){
+// 10ms递归调用自己，刷新数据
+		total_micro_second += 10;
+		timer_cost(that);
+	}
+	,10)
+
+}
+
+// 时间格式化输出，如03:25:19 86。每10ms都会调用一次
+function date_format(micro_second) {
+  	// 秒数
+  	var second = Math.floor(micro_second / 1000);
+	// 天数位
+	var day = Math.floor(second/3600/24);
+  	// 小时位
+  	var hr = Math.floor(second / 3600);
+  	// 分钟位
+  	var min = fill_zero_prefix(Math.floor((second - hr * 3600) / 60));
+  	// 秒位
+	var sec = fill_zero_prefix((second - hr * 3600 - min * 60));// equal to => var sec = second % 60;
+	// 毫秒位，保留2位
+	var micro_sec = fill_zero_prefix(Math.floor((micro_second % 1000) / 10));
+	// time_clock=[day,hr,min,sec];
+	// return time_clock;
+	return  "停车时间："+ day + "天" + hr +"时" + min+"分" + sec +"秒" ;
+}
+
+// 位数不足补零
+function fill_zero_prefix(num) {
+	return num < 10 ? "0" + num : num
+}
+
+Page({
+	data: {
+		Timer: '',
+		start_time:'',
+		total_cost:'',
+		qrcode:'',
+	},
+	onLoad: function() {
+		timer_cost(this);
+		start_time_record(this);
+	},
+	weixin_pay:function(){
+    wx.navigateTo({
+      url: 'succeed_pay/succeed_pay' 
+    })
+  }
+});
---
+++ pages/index/index.js
@@ -0,0 +1,85 @@
+//index.js
+//获取应用实例
+var app = getApp()
+const AV = require('../../utils/av-weapp.js');
+Page({
+  data: {
+    // userInfo:{},
+    indicatorDots:true,
+    autoplay:true,
+    interval:5000,
+    duration:1000,
+    imgUrls:[
+      '../../images/1.png',
+      '../../images/2.png'
+    ]
+  },
+   onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+    AV.init({
+      appId: '6FR9SJu9wGBKXVN95sV48xNB-gzGzoHsz',
+      appKey: 'AIWvKAxqanTFEmMC8vfrtzw5',
+    });
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+      
+    })
+  },
+  Parking_record:function(){
+    wx.navigateTo({
+      url: '../Parking_record/Parking_record',
+     })
+  },
+  Time_fee:function(){
+    wx.navigateTo({
+      url: '../Time_fee/Time_fee',
+      })
+  },
+  choose_image:function(){
+    
+    var _this = this;
+    var fileurl = null;
+    wx.chooseImage({
+      count: 9, // 默认9  
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有  
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有  
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片  
+        _this.setData({
+          tempFilePaths: res.tempFilePaths
+        })
+        var tempFilePath = res.tempFilePaths[0];
+        new AV.File('file-name', {
+          blob: {
+            uri: tempFilePath,
+          },
+        }).save().then(
+          // fileurl = file.url(),
+          function (file) {
+            // src: file.url()
+            wx.request({
+              url: 'https://api.qrserver.com/v1/read-qr-code/',
+              data: {
+                fileurl: file.url()
+              },
+              header: {
+                'content-type': 'application/json'
+              },
+              method: 'GET',
+              success: function (res) {
+                console.log(res.data[0].symbol[0].data)
+                wx.navigateTo({
+                  url: '../Time_fee/Time_fee',
+                })
+              }
+            })
+            return console.log(file.url());
+          },
+        ).catch(console.error);
+      }
+    })
+  },  
+})
